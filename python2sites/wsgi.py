7e660f880501d38dfcda4075c4a2086405520578
electrocoder <electrocoder@gmail.com>
2016-09-27 22:47:44 +0300

    import sys

--- python2sites/wsgi.py
+++ python2sites/wsgi.py
@@ -8,10 +8,15 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 from dj_static import Cling
+
+path = '/home/django'
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "python2sites.settings")
 
