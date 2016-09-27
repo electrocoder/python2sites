6a0d954310f3a889eb389e6ddea105e82ba2669c
electrocoder <electrocoder@gmail.com>
2016-09-27 22:02:58 +0300

    python2sites

--- python2sites/wsgi.py
+++ python2sites/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "python2sites.settings")
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(application)
